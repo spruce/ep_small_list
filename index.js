7f79a63320bef7d4778acbb0b303cc8abff5ce12
Adrien Bustany <adrien@bustany.org>
2013-05-14 16:32:05 +0200

    Lint the code a bit

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var padManager = require('ep_etherpad-lite/node/db/PadManager');
 exports.eejsBlock_indexWrapper = function (hook_name, args, cb) {
-
-    padstring = "<h3>List of pads</h3>";
-
-    pads = padManager.listAllPads().padIDs;
+    var padstring = "<h3>List of pads</h3>";
+    var pads = padManager.listAllPads().padIDs;
     pads = pads.sort();
     padstring += "<ul>";
     pads.forEach(function(item){
@@ -11,8 +9,6 @@ exports.eejsBlock_indexWrapper = function (hook_name, args, cb) {
     });
     padstring += "</ul>";
     args.content += padstring
-/*<% pads.forEach(function (pad) { %>
-          <div class="pad"><a href="/public/<%= pad.toString() %>"><%= pad.toString() %></a></div>
-       <% }) %>*/
-  return cb();
+
+    return cb();
 }
