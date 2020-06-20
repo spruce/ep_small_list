037f23c6e384e8221776be238c663410cf235242
Florian Rittmeier <florianr@uni-paderborn.de>
2020-06-20 02:01:33 +0200

    added setting that allows to sort list by creation or by last change

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 var padManager = require('ep_etherpad-lite/node/db/PadManager');
+var settings = require('ep_etherpad-lite/node/utils/Settings');
+
+const sortmodes = {
+    CREATION: 'creation',
+    LASTCHANGE: 'lastchange'
+}
+
+var sortmode = sortmodes.CREATION;
+
+if (settings.ep_small_list) {
+    if (settings.ep_small_list.sortmode) {
+      sortmode = settings.ep_small_list.sortmode.trim().toLowerCase() === sortmodes.LASTCHANGE ? sortmodes.LASTCHANGE : sortmodes.CREATION;
+      console.log("ep_small_list setting sortmode: " + sortmode);
+    }
+}
+
 exports.eejsBlock_indexWrapper = function(hook_name, args, cb) {
     args.content += '<div id="small_list"></div><script src="./static/js/jquery.js"></script><script>$(function () { $("#small_list").load("./small_list"); });</script>';
 };
@@ -14,12 +30,39 @@ exports.registerRoute = function(hook_name, args, cb) {
 };
 
 async function createList(data){
-  r = "<ul>";
+  let dataCache = [];
   if(data && data.padIDs){
     for (var i = 0; i < data.padIDs.length; i++) {
-        r += '<li><a href="./p/' + data.padIDs[i] + '">' + data.padIDs[i] + '</li>';
+      let padData = {};
+      let padID = data.padIDs[i];
+
+      const pad = await padManager.getPad(padID);
+      if (!pad) {
+        continue;
+      }
+      const lastEdit = await pad.getLastEdit();
+      padData.lastChange = lastEdit;
+
+      padData.html = '<li><a href="./p/' + padID + '">' + padID + '</li>';
+      padData.createIndex = i;
+
+      dataCache.push(padData);
     }
   }
+
+  dataCache.sort(function (a, b) {
+    let indicator = 0;
+    if(sortmode === sortmodes.CREATION) {
+      indicator = a.createIndex - b.createIndex;
+    } else {
+       // sortmodes.LASTCHANGE
+      indicator = a.lastChange - b.lastChange;
+    }
+    return indicator;
+  });
+
+  let r = "<ul>";
+  r += dataCache.map(x => x.html).join("");
   r += "</ul>";
   return r;
 }
